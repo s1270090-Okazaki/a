c69a59968c140701bedf4aed9acaaa2fee6f7f89
s1270090-Okazaki <49747862+s1270090-Okazaki@users.noreply.github.com>
2021-12-06 05:39:09 +0900

    Update test_Palindromes.cpp

--- palindromes/test_Palindromes.cpp
+++ palindromes/test_Palindromes.cpp
@@ -8,22 +8,6 @@ using std::string;
 using std::cin;
 
 int main(){
-  string s1 = "abcdcba";
-  string s2 = "abcdefg";
-
-  cout << "s1 : " << s1 << endl;
-  if(isPalindrome(s1)){
-    cout << "s1 is a palindrome" << endl;
-  } else {
-    cout << "s1 is not a palindrome" << endl;
-  }
-
-  cout << "s2 : " << s2 << endl;
-  if(isPalindrome(s2)){
-    cout << "s2 is a palindrome" << endl;
-  } else {
-    cout << "s2 is not a palindrome" << endl;
-  }
 
   string input;
   while(1){
